7fd4083a2d2ea03fcb143da86466554bfaea9914
Francisco Rappazzini <fran_rappa@outlook.com>
2022-09-01 16:17:55 -0300

    feat (server): GET /product queries added

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,16 +3,31 @@ const { pool } = require("../db");
 const product = Router();
 
 product.get("", (req, res) => {
-  const { name, category } = req.query;
+  const { name, category, order } = req.query;
 
+  const orderBy = order?.split("-");
+
+  console.log(orderBy);
+
+  // TODO pasar a switch
   let query = "";
-  if (name && category) {
+  if (name && category && order) {
+    query = `SELECT * FROM product WHERE name LIKE '%${name}%' AND category=${category} ORDER BY ${orderBy[0]} ${orderBy[1]};`;
+  } else if (name && order) {
+    query = `SELECT * FROM product WHERE name LIKE '%${name}%' ORDER BY ${orderBy[0]} ${orderBy[1]};`;
+  } else if (category && order) {
+    query = `SELECT * FROM product WHERE category=${category} ORDER BY ${orderBy[0]} ${orderBy[1]};`;
+  } else if (name && category) {
     query = `SELECT * FROM product WHERE name LIKE '%${name}%' AND category=${category};`;
   } else if (name) {
     query = `SELECT * FROM product WHERE name LIKE '%${name}%';`;
   } else if (category) {
     query = `SELECT * FROM product WHERE category=${category};`;
+  } else if (order) {
+    query = `SELECT * FROM product ORDER BY ${orderBy[0]} ${orderBy[1]};`;
   } else query = "SELECT * FROM product;";
+
+  console.log(query);
 
   try {
     pool.getConnection((err, conn) => {
